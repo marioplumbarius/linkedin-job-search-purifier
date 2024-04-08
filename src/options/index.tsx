cfce39476f14334bc6bce2645362a79a2c3dd420
marioluan <mariosouzaluan@gmail.com>
2024-04-09 00:52:58 +1000

    move options form to components

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
+import App from "../components/Options";
 
 const root = ReactDOM.createRoot(document.getElementById("app")!);
 
