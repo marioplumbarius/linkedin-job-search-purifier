075f400bc338a0680971c1e113f84b6acb0fc288
marioluan <mariosouzaluan@gmail.com>
2024-04-13 18:55:44 +1000

    wip - parse job details

--- src/content.ts
+++ src/content.ts
@@ -1,1 +1,44 @@
+import { JobParser } from "./dom-parsers";
+import { Job, JobCompany, JobSetup, JobType } from "./dto";
 console.info("content script was executed!");
+
+enum TimeUnit {
+  Millisecond = 1000,
+}
+
+function fetchJobDataFromDOM(jobParser: JobParser, delayForSeconds?: number) {
+  const fetch = () => {
+    const job = jobParser.parse();
+    console.log(`Job: ${JSON.stringify(job, undefined, 2)}`);
+  };
+
+  if (delayForSeconds)
+    setTimeout(fetch, delayForSeconds * TimeUnit.Millisecond);
+  else fetch();
+}
+
+function registerClickToOpenJobPosting(
+  jobParser: JobParser,
+  everySeconds?: number,
+) {
+  const register = () => {
+    const $jobCardContainers = document.querySelectorAll(".job-card-container");
+    $jobCardContainers.forEach(($jobCardContainer) => {
+      // remove first, to avoid duplicates
+      $jobCardContainer.removeEventListener("click", () =>
+        fetchJobDataFromDOM(jobParser, 1),
+      );
+      $jobCardContainer.addEventListener("click", () =>
+        fetchJobDataFromDOM(jobParser, 1),
+      );
+    });
+  };
+
+  if (everySeconds) setInterval(register, everySeconds * TimeUnit.Millisecond);
+  else register();
+}
+
+const jobParser = new JobParser();
+
+fetchJobDataFromDOM(jobParser, 1);
+registerClickToOpenJobPosting(jobParser, 2);
--- src/dto.ts
+++ src/dto.ts
@@ -8,3 +8,46 @@ export interface ExtensionOptions {
 export const DefaultExtensionOptions: ExtensionOptions = {
   denyList: { titles: [], companies: [] },
 };
+
+export enum JobType {
+  FullTime = "Full-time",
+  Contract = "Contract",
+  PartTime = "Part-time",
+}
+
+export enum JobSetup {
+  Remote = "Remote",
+  OnSite = "On-site",
+  Hybrid = "Hybrid",
+}
+
+export interface JobCompany {
+  name: string;
+  description: string;
+  followers: string;
+}
+
+export interface JobLocation {
+  country: string;
+  // country+state+city
+  full: string;
+}
+
+export interface Job {
+  applies: number;
+  company: JobCompany;
+  // TODO: descriptions are usually long. What signals do we need from description in form of tags?
+  description: string;
+  location: JobLocation;
+  // TODO: ensure description doesn't mention it's a contract with possibility to become full time
+  setup: JobSetup;
+  // TODO: match with 'desired' skills, instead of Linkedin's "all skills" from profile
+  skills: string[];
+  title: string;
+  // TODO: make sure the description matches this and doesn't call it remote, but it's hybrid
+  type: JobType;
+  url: URL;
+  // TODO: compute a metric for applies vs. views. If many people viewed it but didn't apply
+  // that's prob. a red flag or skills are very specific?
+  views: number;
+}
